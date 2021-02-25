11a7681869445171194ef5b0c9d3d249787a7c06
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-02-25 11:12:30 +0200

    added json

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,180 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500;1,600&display=swap');
+
+ :root {
+    --header-height: 3rem;
+
+    /* ==== COLORS ==== */
+    --first-color: #3e0e12;
+    --first-color-dark: #2f0a0d;
+    --text-color: #524748;
+    --first-color-light: #7b6f71;
+    --first-color-lighten: #fbf9f9;
+
+    /* Font and Typography */
+    --body-font: 'Poppins', sans-serif;
+    --biggest-font-size: 2.5rem;
+    --h1-font-size: 1.5rem;
+    --h2-font-size: 1.25rem;
+    --h3-font-size: 1.125rem;
+    --normal-font-size: .938rem;
+    --small-font-size: .813rem;
+    --smaller-font-size: .75rem;
+
+    /* Font weight */
+    --font-medium: 500;
+    --font-semi-bold: 600;
+    --font-bold: 700;
+
+    /* Margins */
+    --mb-1: .5rem;
+    --mb-2: 1rem;
+    --mb-3: 1.5rem;
+    --mb-4: 2rem;
+    --mb-5: 2.5rem;
+    --mb-6: 3rem;
+
+    /* z-index */
+    --z-normal: 1;
+    --z-tooltip: 10;
+    --z-fixed: 100;
+}
+
+@media screen and (min-width: 768px) {
+    :root {
+        --biggest-font-size: 4.5rem;
+        --h1-font-size: 2.25rem;
+        --h2-font-size: 1.5rem;
+        --h3-font-size: 1.25rem;
+        --normal-font-size: 1rem;
+        --small-font-size: .875rem;
+        --smaller-font-size: .813rem;
+    }
+}
+
+/* ==== GLOBAL CSS ==== */
+*,::before,::after {
+    box-sizing: border-box;
+}
+
+html {
+    scroll-behavior: smooth;
+}
+
+body {
+    margin: var(--header-height) 0 0 0;
+    font-family: var(--body-font);
+    font-size: var(--normal-font-size);
+    font-weight: var(--font-medium);
+    background-color: var(--first-color-lighten);
+    color: var(--text-color);
+    line-height: 1.6;
+}
+
+h1,h2,h3,ul,p {
+    margin: 0;
+}
+
+h2,h3 {
+    font-weight: var(--font-semi-bold);
+}
+
+ul {
+    padding: 0;
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+}
+
+img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+}
+
+/* ==== CLASS CSS ==== */
+.section {
+    padding: 4rem 0 2rem;
+}
+
+.section-title, .section-subtitle {
+    text-align: center;
+}
+
+.section-title {
+    font-size: var(--h1-font-size);
+    color: var(--first-color);
+    margin-bottom: var(--mb-3);
+}
+
+.section-subtitle {
+    display: block;
+    font-size: var(--smaller-font-size);
+    font-weight: var(--font-semi-bold);
+}
+
+/* ==== LAYOUT ==== */
+.bd-container {
+    max-width: auto;
+    width: calc(100% - 2rem);
+    margin-left: var(--mb-2);
+    margin-right: var(--mb-2);
+}
+
+.bd-grid {
+    display: grid;
+    gap: 1.5rem;
+}
+
+.l-header {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: var(--z-fixed);
+    background-color: #000;
+}
+
+
+.button {
+    display: inline-block;
+    background-color: var(--first-color);
+    color: var(--first-color-lighten);
+    padding: .75rem 1rem;
+    border-radius: .25rem;
+    transition: .3s;
+}
+
+.button:hover {
+    background-color: var(--first-color-dark);
+}
+
+.button-light {
+    background-color: var(--first-color-light);
+}
+
+.button-white {
+    background-color: var(--first-color-lighten);
+    color: var(--first-color-dark);
+}
+
+.button-white:hover {
+    background-color: var(--first-color-lighten);
+}
+
+.button-link {
+    background: none;
+    color: var(--first-color-dark);
+    padding: 0;
+}
+
+.button-link:hover {
+    background: none;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,98 @@
+<template>
+    <section class="home" id="home">
+            <div class="home_container bd-container b-grid">
+                <div class="home_data">
+                    <span class="home_greeting">Hi, My name is</span>
+                    <h1 class="home_name">Sicelo Sitsha</h1>
+                    <span class="home_profession">Web Developer</span>
+                    <a download="" href="assets/pdf/Sicelo Sitsha resume.pdf" class="button button-light">Download CV</a>
+                </div>
+
+                <div class="home_social" id="home-social" :key="data" v-for="data in myJson">
+                      <i class="{{data.home}}"></i>
+                </div>
+
+                <div class="home_img">
+                    <img src="https://images.pexels.com/photos/3568520/pexels-photo-3568520.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+                </div>
+            </div>
+        </section>
+</template>
+<script>
+
+import json from '../constants.json'
+export default {
+  data() {
+    return{
+      myJson: json
+    }
+  }
+}
+</script>
+<style scoped>
+.home {
+    background: #000;
+    overflow: hidden;
+}
+
+.home_container {
+    position: relative;
+    height: 90vh;
+    grid-template-rows: repeat(2, max-content);
+    align-content: space-around;
+    row-gap: 2rem;
+    margin-top: var(--header-height);
+}
+
+.home_data {
+    border-left: 4px solid var(--first-color-lighten);
+    color: var(--first-color-lighten);
+    padding-left: 1.5rem;
+    z-index: var(--z-tooltip);
+    margin-bottom: 5rem;
+}
+
+.home_name {
+    font-size: var(--biggest-font-size);
+}
+
+.home_greeting, .home_profession {
+    display: block;
+    font-weight: var(--font-bond);
+}
+
+.home_greeting {
+    font-size: .813rem;
+}
+
+.home_profession {
+    font-size: .938rem;
+    margin-bottom: var(--mb-3);
+}
+
+.home_img {
+    position: absolute;
+    right: 0;
+    bottom: 4rem;
+}
+
+.home_img img {
+    width: 200px;
+}
+
+.home_social {
+    display: flex;
+    flex-direction: column;
+}
+
+.home_social-icon {
+    width: max-content;
+    font-size: 1.3rem;
+    margin-bottom: var(--mb-3);
+    color: var(--first-color-lighten);
+}
+
+.home_social-icon:hover {
+    color: var(--first-color-light);
+}
+</style>
