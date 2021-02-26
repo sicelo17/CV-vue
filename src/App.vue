260736b2586f25fa480a388bf2966281945615aa
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-02-26 08:57:20 +0200

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
+  <Header/>
   <router-view/>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+     Header },
+  
+}
+</script>
+
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500;1,600&display=swap');
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="l-header" id="header">
+        <nav class="nav bd-container">
+            <a href="#" class="nav_logo">Sicelo Sitsha</a>
+
+            <div class="nav_menu" id="nav-menu">
+                <ul class="nav_list">
+                    <li class="nav_item"><a href="#home" class="nav_link active-link">Home</a></li>
+                    <li class="nav_item"><a href="#about" class="nav_link">About</a></li>
+                    <li class="nav_item"><a href="#services" class="nav_link">Services</a></li>
+                    <li class="nav_item"><a href="#portfolio" class="nav_link">Portfolio</a></li>
+                    <li class="nav_item"><a href="#contact" class="nav_link">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="nav_toggle" id="nav-toggle">
+                <i class='bx bx-menu'></i>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.nav {
+    height: var(--header-height);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.nav_item {
+    margin-bottom: var(--mb-3);
+}
+
+.nav_link {
+    color: var(--first-color-lighten);
+    transition: .3s;
+}
+
+.nav_link:hover {
+    color: var(--first-color-light);
+}
+
+.nav_logo, .nav_toggle {
+    color: var(--first-color-lighten);
+}
+
+.nav_toggle {
+    font-size: 1.3rem;
+    cursor: pointer;
+}
+
+</style>
